79eb326d9dd4f0222183004694b40cee0af72caf
Burhanuddin Kader <burhankader5252@gmail.com>
2022-09-12 23:21:56 +0530

    setup tested

--- quiz/home/views.py
+++ quiz/home/views.py
@@ -1,6 +1,6 @@
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HTTPResponse("Hello my name is Bk")
+    return HttpResponse("Hello my name is Bk")
