766e080f1087698a51c5c0939e6e911fee44a286
Burhanuddin Kader <burhankader5252@gmail.com>
2022-09-12 23:42:14 +0530

    Added parent model

--- quiz/home/models.py
+++ quiz/home/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    uid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now_add=True)
